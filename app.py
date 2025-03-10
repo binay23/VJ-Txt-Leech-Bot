2ce238a2fb9b1cf36f1f6ac6c724c0f44497c768
binay23 <91482482+binay23@users.noreply.github.com>
2025-03-10 22:25:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Tech VJ'
 
-
+# Run the app on all available interfaces (0.0.0.0) to allow external access
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
